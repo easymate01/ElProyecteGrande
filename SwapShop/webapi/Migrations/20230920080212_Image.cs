6d280eaf7cf7cef4ea8cfcc80434b9230867f414
Ati <nagyonkellvalamihez@gmail.com>
2023-09-20 10:08:34 +0200

    Create migrations

---
+++ SwapShop/webapi/Migrations/20230920080212_Image.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Image : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageBase64",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageBase64",
+                table: "Products");
+        }
+    }
+}
